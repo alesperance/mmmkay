72fec2903935101f6da4f9673ac3f39faa19ae01
Alex L'Esperance <alesperance22@gmail.com>
2015-07-29 21:30:36 -0500

    Initial Commit

---
+++ Mmmkay/Mkay.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mmmkay
+{
+    class Mkay
+    {
+        private Random r = new Random();
+        private string input { get; set; }
+
+        public Mkay(string input)
+        {
+            //Constructs all the things
+            this.input = input;
+        }
+
+
+        public string getMkay(int num)
+        {
+            if (num == 0)
+                return "m'kay";
+            else if (num == 1)
+                return "mm'kay";
+            else return "mmm'kay";
+        }
+
+        public string getMKAY(int num)
+        {
+            if (num == 0)
+                return "M'kay";
+            else if (num == 1)
+                return "Mm'kay";
+            else return "Mmm'kay";
+        }
+
+        public void computeString()
+        {
+            string[] parts = this.input.Split(null);
+            List<string> text = parts.ToList<string>();
+            StringBuilder sb = new StringBuilder();
+            bool found = false;
+            List<int> places = new List<int>();
+            
+            for (int i = 0; i < parts.Length; i++ )
+            {
+                string temp = parts[i];
+                if(temp.Contains(','))
+                {
+                    if(FiftyFifty())
+                    {
+                        text[i] += " " + getMkay(r.Next(0, 3)) + ", ";
+                        found = true;
+                    }
+                    else
+                    {
+                        places.Add(i);
+                    }
+                }
+                else if (temp.Contains('.') || temp.Contains('?') || temp.Contains('!'))
+                {
+                    if (FiftyFifty())
+                    {
+                        char end = temp[temp.Length - 1];
+                        text[i] += " " + getMKAY(r.Next(0, 3)) + end + " ";
+                        found = true;
+                    }
+                    else
+                    {
+                        places.Add(i);
+                    }
+                }
+
+                text[i] += " ";
+            }
+               
+            if(!found)
+            {
+                int position = r.Next(0, places.Count);
+                if(parts[position].Contains(','))
+                    text[position] += getMkay(r.Next(0, 3)) + ", ";
+                else
+                {
+                    char end = parts[position].ElementAt(parts.Length - 1);
+                    text[position] += getMKAY(r.Next(0, 3)) + end + " ";
+                }
+            }
+            foreach(string s in text)
+            {
+                Console.Write(s);
+            }
+        }
+
+        public bool FiftyFifty()
+        {
+            if (r.Next(0, 2) == 0)
+                return false;
+            else return true;
+        }
+    }
+}
---
+++ Mmmkay/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mmmkay
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            string input;
+            
+            while(true)
+            {
+                Console.Clear();
+                SplashScreen();
+                Menu();
+                Console.Write("Enter Text: ");
+                input = Console.ReadLine();
+                Console.Clear();
+                SplashScreen();
+                if (input.Equals("-h"))
+                {
+                    input = "Drugs are bad, so, if you do drugs, you're bad, because drugs are bad. They can hurt your body, cause drugs are bad.";
+                    Console.WriteLine("Example input: " + input + "\n\n");
+                    System.Threading.Thread.Sleep(3000);
+                }
+                if (input.Equals("-q"))
+                    break;
+                Mkay mkay = new Mkay(input);
+                mkay.computeString();
+                Console.ReadLine();
+            }
+            Console.Clear();
+            SplashScreen();
+            Console.WriteLine("Have a good day you awesome individual!");
+            Console.WriteLine("© 2015 Alex L'Esperance");
+            System.Threading.Thread.Sleep(5000);
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Welcome to the Mr. Mackey-a-fied Text Translator.  Enter your text below and let Mr. Mackey show you how it's done mmm'kay?\n");
+            Console.WriteLine("Type -h for an example");
+            Console.WriteLine("Type -q to quit\n");
+        }
+
+        static void SplashScreen()
+        {
+            Console.WriteLine("   _     _     _     _     _     '     _     _     _     _     _     _   ");
+            Console.WriteLine("  (c).-.(c)   (c).-.(c)   (c).-.(c)   (c).-.(c)   (c).-.(c)   (c).-.(c)  ");
+            Console.WriteLine("   / ._. \\     / ._. \\     / ._. \\     / ._. \\     / ._. \\     / ._. \\   ");
+            Console.WriteLine(" __\\( Y )/__ __\\( Y )/__ __\\( Y )/__ __\\( Y )/__ __\\( Y )/__ __\\( Y )/__ ");
+            Console.WriteLine("(_.-/'-'\\-._(_.-/'-'\\-._(_.-/'-'\\-._(_.-/'-'\\-._(_.-/'-'\\-._(_.-/'-'\\-._)");
+            Console.WriteLine("   || M ||     || M ||     || M ||     || K ||     || A ||     || Y ||   ");
+            Console.WriteLine(" _.' `-' '._ _.' `-' '._ _.' `-' '._ _.' `-' '._ _.' `-' '._ _.' `-' '._ ");
+            Console.WriteLine("(.-./`-'\\.-.(.-./`-'\\.-.(.-./`-'\\.-.(.-./`-'\\.-.(.-./`-'\\.-.(.-./`-'\\.-.)");
+            Console.WriteLine(" `-'     `-' `-'     `-' `-'     `-' `-'     `-' `-'     `-' `-'     `-' ");
+            Console.WriteLine("");
+        }
+    }
+}
